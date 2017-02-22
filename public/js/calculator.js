ce94b1fcac165f9de3ed78e776cf0616daa15581
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-02-22 10:58:57 -0600

    please look at my calculator. 02222017

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,10 +1,75 @@
 "use strict";
 
-var leftInput = function(event) {
-	var inputNumbers = document.getElementsByClassName("numbers");
+var operand = "left-operand";
 
-	for (var i = 0; i < inputNumbers.length; i++) {
-		var leftButton = inputNumbers[i].getAttribute("value");
-	}
+// Functions
+var appendValue = function(event) {
+  document.getElementById(operand).value += this.value;
+};
+var clearValue = function(event) {
+  var val = document.getElementById(operand).value;
+
+  if (val === "") {
+    document.getElementById("operator").value = "";
+    operand = "left-operand";
+  }
+
+  document.getElementById(operand).value = "";
+  document.getElementById("evaluation").value = "";
+};
+var evaluate = function(left, right, operator) {
+  var evaluation;
+  switch (operator) {
+    case "+":
+      evaluation = (left + right);
+      break;
+    case "-":
+      evaluation = (left - right);
+      break;
+    case "*":
+      evaluation = (left * right);
+      break;
+    case "/":
+      evaluation = (left / right);
+      break;
+    default:
+      evaluation = 0;
+  }
+  document.getElementById("left-operand").value = evaluation;
+  document.getElementById("right-operand").value = "";
 }
-document.getElementById("left_operand").addEventListener("click", leftInput);
+
+// Setup Clear
+var clear = document.getElementById("clear");
+clear.addEventListener("click", clearValue);
+
+// Setup Numbers
+var numbers = document.getElementsByClassName("numbers");
+for (var i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", appendValue);
+}
+
+// Setup operators
+document.getElementById("add").addEventListener("click", function(event) {
+  document.getElementById("operator").value = this.value;
+  operand = "right-operand";
+});
+document.getElementById("subtract").addEventListener("click", function(event) {
+  document.getElementById("operator").value = this.value;
+  operand = "right-operand";
+});
+document.getElementById("multiply").addEventListener("click", function(event) {
+  document.getElementById("operator").value = this.value;
+  operand = "right-operand";
+});
+document.getElementById("divide").addEventListener("click", function(event) {
+  document.getElementById("operator").value = this.value;
+  operand = "right-operand";
+});
+// Setup equals
+document.getElementById("equals").addEventListener("click", function(event) {
+  var left = Number(document.getElementById("left-operand").value);
+  var right = Number(document.getElementById("right-operand").value);
+  var operator = document.getElementById("operator").value;
+  evaluate(left, right, operator);
+});
